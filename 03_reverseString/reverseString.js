e4a1281f5d75e1c77ab5f1da3cad5b7ef675f2f5
Clonosaurio <96928935+Clonosaurio@users.noreply.github.com>
2022-03-08 17:41:23 -0300

    shrinked code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
 const reverseString = function(str) {
     let reversed = "";
     for(let i = (str.length-1); i >= 0; i--){
-        reversed = reversed+str[i];
+        reversed +=  str[i];
     };
     return reversed;
 };
