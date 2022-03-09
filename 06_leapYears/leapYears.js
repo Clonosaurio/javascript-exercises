6d1316cee97e45cda53d2f52329a37584a0de2b7
Clonosaurio <96928935+Clonosaurio@users.noreply.github.com>
2022-03-09 18:42:01 -0300

    solved 6th exercise, but not meeting conditions yet

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //year%4==0   -> true
+    //year%100==0 -> false
+    //but!
+    //year%100==0 && year%400==0 -> true
 
+    if(year % 4 === 0){
+        if(year % 100 === 0){
+            if(year % 400 === 0){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
