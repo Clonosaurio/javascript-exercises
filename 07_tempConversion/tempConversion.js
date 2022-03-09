395fe5ce48ebe72c7dcb5a031375f589aedd1923
Clonosaurio <96928935+Clonosaurio@users.noreply.github.com>
2022-03-09 19:47:36 -0300

    solved 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+const ftoc = function(fah) {
+  let cel = ((fah-32)*5)/9;
 
+  if(cel == Math.floor(cel)){
+    return cel;
+  }else{
+    return parseFloat(cel.toFixed(1));
+  }
 };
 
-const ctof = function() {
+const ctof = function(cel) {
+  let fah = (cel*9/5)+32;
 
+  if(fah == Math.floor(fah)){
+    return fah;
+  }else{
+    return parseFloat(fah.toFixed(1));
+  }
 };
 
 // Do not edit below this line
