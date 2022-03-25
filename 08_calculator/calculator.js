0fed2c77ac763c4643900aa65f3c5e2cf2ed4e93
Clonosaurio <96928935+Clonosaurio@users.noreply.github.com>
2022-03-25 14:46:43 -0300

    solved 8th exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(...n) {
+	let total = 0
+  n.map(num => total += num);
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, ...n2) {
+	let total = n1;
+  n2.map(num => total -= num);
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length == 0) {
+    return 0;
+    }
+  let total = 0;
+  arr.map(num => total += parseInt(num));
+  return total;  
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  arr.map(n => total *= n);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(n, p) {
+	// return n**p;
+  total = n;
+  for (let i = 1; i < p; i++) {
+    total = total * n;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  total = 1;
+  for(let i = 1; i <= n; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
