4234bfad6eb66e7c7d7ff354cdc99fd866f0950c
Clonosaurio <96928935+Clonosaurio@users.noreply.github.com>
2022-03-25 15:46:36 -0300

    solved 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanStr = "";
+    let reversed = "";
+    let excluded = "., !";
 
+     str.split("").map(char => {
+        if(!excluded.includes(char)){
+            cleanStr += char;
+            reversed = char+reversed;
+        }
+    });
+    
+    return cleanStr.toLowerCase() == reversed.toLowerCase();
 };
 
 // Do not edit below this line
