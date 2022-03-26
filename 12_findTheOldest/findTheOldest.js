e257285f836fb905ada6d20ced0c5938452bcd4a
Clonosaurio <96928935+Clonosaurio@users.noreply.github.com>
2022-03-26 17:45:03 -0300

    solved 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    //done without "reduce" function, no idea how to use it in this case
+    const sorted = arr.sort((a, b) => {
+        let aLived = stillAlive(a);
+        let bLived = stillAlive(b);
+    return aLived > bLived ? -1: 1;
+  });
+  return sorted[0];
 };
+
+function stillAlive(person){
+    const actualYear = new Date().getFullYear();
+    return person.yearOfDeath == undefined ? aLived = actualYear - person.yearOfBirth : aLived = person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
