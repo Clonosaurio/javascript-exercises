e7dc9dfabeb221929919a35af32f0daa43b74e53
Clonosaurio <96928935+Clonosaurio@users.noreply.github.com>
2022-03-26 14:59:40 -0300

    solved 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    //feels too basic, maybe I missed the point and skipped something
+    //I don't know which advanced methods I should use here
+    if(n < 0){
+        return "OOPS";
+    };
+    let sequence = [1,1];
+    for(let i=0; i < n-2; i++){
+      let length = sequence.length;
+        sequence.push( parseInt(sequence[length-1] + sequence[length-2]));
+    };
+    return sequence[sequence.length-1];
 };
 
 // Do not edit below this line
